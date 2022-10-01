f0505a83a6b5d0eeabd4baee682d245f75309d7e
Warren 'Solber' Noth <eliacrah@gmail.com>
2022-10-02 00:59:38 +0200

    initial commit

---
+++ server/routes/api/chat.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mongoDB = require('mongodb')
+const {ObjectID} = require("mongodb");
+
+const router = express.Router()
+
+// GET Messages
+router.get('/messages', async (req, res) => {
+    const chat = await loadChatCollection()
+
+    res.send(await chat.find({}).toArray())
+})
+
+// POST Message
+router.post('/message', async (req, res) => {
+    const chat = await loadChatCollection()
+
+    await chat.insertOne({
+        message: req.body.message,
+        username: req.body.username,
+        sendAt: req.body.sendAt,
+    })
+
+    res.status(201).send()
+})
+
+// DELETE Message
+router.delete('/message/:id', async (req, res) => {
+    const chat = await loadChatCollection()
+
+    await chat.deleteOne({ _id: ObjectID(req.params.id) })
+
+    res.status(200).send()
+})
+
+async function loadChatCollection()
+{
+    const client = await mongoDB.MongoClient.connect('mongodb://localhost:27017/chocket', {
+        useNewUrlParser: true,
+    })
+
+    return client.db('chocket').collection('chat')
+}
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { Server } = require('socket.io')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'REMOVE']
+    }
+})
+const PORT = process.env.PORT || 8080
+
+// Routes mapping
+app.use('/api/chat', CHAT)
+
+// Handle prod
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public'))
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
+// Routes
+const CHAT = require('./routes/api/chat')
+
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+io.on('connection', (socket) => {
+    console.log('New socket user')
+
+    socket.on('SEND_MESSAGE', data => {
+        console.log('received message in back')
+        io.emit('MESSAGE', data)
+    })
+})
+
+
+server.listen(PORT, () => { console.log(`Server started on port : ${PORT}`)})
